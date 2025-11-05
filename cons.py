77af530b46a5f65a028f3d025443b01a96f334a0
mindChickey <w453908766@126.com>
2025-11-05 16:55:33 +0800

    add cons.py

---
+++ cons.py
@@ -0,0 +1,13 @@
+from mcons import batch, ConsModule, cons_object, task
+from cons_config import Config
+from .Obfuscation.cons import cons_Obfuscation_a
+
+cm = ConsModule(__file__)
+
+def cons_AddPass_o(config: Config):
+  return cons_object(cm, "AddPass.o", "AddPass.cpp", config.compile_templ)
+
+def cons_Obfuscator_so(config: Config):
+  deps = batch([cons_AddPass_o, cons_Obfuscation_a], config)
+  cmd = f"{config.cxx} -fuse-ld={config.ld} -fPIC -shared {config.link_flags} {{_deps}} -o {{_target}}"
+  return task(cm, "libObfuscator.so", deps, cmd)
